f03606500798233942ac862d3ec70c13833602db
Carlos Mairena <carlosmiguelmairena@gmail.com>
2021-12-14 12:00:13 -0600

    feat: add routes folder

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,25 @@
 import * as express from "express";
 import * as helmet from "helmet";
+import * as cors from "cors";
+import ROUTER from "./routes";
 
 const PORT = process.env.PORT || 3000;
 
 try {
     
     const app = express();
-    
-    // middlewares
-    //app.use(cors());
+
     app.use(helmet());
     app.use(express.json());
+    app.use(cors());
     
     // routes
-    //app.use("/api", ROUTER);
+    app.use("/", ROUTER);
     
     // start express server
     app.listen(PORT);
     
-    console.log(`The server has started on port ${PORT}. Open http://localhost:${PORT}/api/users to see results`);
+    console.log(`The server has started on port ${PORT}. Open http://localhost:${PORT}/ to see results`);
 } catch (error) {
-    
+    console.error(error);
 }
