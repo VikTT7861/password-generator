9b2f725a5f151f7fcbf2999ca1ca419553aee2e4
Victor Zuniga <victorandres1218@gmail.com>
2021-12-23 12:48:07 -0600

    feat: Add additional features in the navbar

---
+++ src/static/js/dropdown-menu.js
@@ -0,0 +1,16 @@
+// Functionality of the dropdown menu
+document.addEventListener('click', e => {
+    const isDropDownButton = e.target.matches('[data-dropdown-button]');
+    if (!isDropDownButton && e.target.closest('[data-dropdown]') != null) return
+
+    let currentDropdown;
+    if (isDropDownButton) {
+        currentDropdown = e.target.closest('[data-dropdown]');
+        currentDropdown.classList.toggle('active');
+    }
+
+    document.querySelectorAll('[data-dropdown].active').forEach(dropdown => {
+        if (dropdown === currentDropdown) return
+        dropdown.classList.remove('active');
+    });
+});
