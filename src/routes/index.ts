bdcc9c12ef8c2d3a1968b077c2c082dd7e46fe4d
Victor Zuniga <victorandres1218@gmail.com>
2021-12-26 19:56:19 -0600

    feat(index),fix(darkmode): Add an empty principal page and fix the colors for the dark mode in the navbar and footer

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,12 +2,13 @@ import { NextFunction, Request, Response, Router } from "express";
 
 const ROUTER = Router();
 
+
 ROUTER.get('/', (request: Request, response: Response) => {
-    // return response.status(200).json({ message: 'Server works!' });
-    // return response.sendFile('index.ejs', {
-    //     root: path.join(__dirname, '../view/')
-    // });
     return response.render('index');
+});
+
+ROUTER.get('/password', (request: Request, response: Response) => {
+    return response.render('password');
 });
 
 export default ROUTER;
