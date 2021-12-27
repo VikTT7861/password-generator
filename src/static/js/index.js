dda29e0e3c12c8dfd190b38c12dee892fd4feeda
Victor Zuniga <victorandres1218@gmail.com>
2021-12-27 17:39:35 -0600

    feat,fix(navbar,darkmode): Slipt the files for a better understanding and fix the navbar dimensios and darkmode applied to all pages.

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -56,31 +56,7 @@ const generatePassword = (lower, upper, number, symbol, length) => {
 
 /* Creating the copy to clipboard function */
 let button = document.getElementById('clipboardBtn');
-button.addEventListener('click', (e) => {
+button.addEventListener('click', e => {
     e.preventDefault();
     document.execCommand('copy', false, document.getElementById('PasswordResult').select());
 });
-
-/* Dark mode toggle function */
-const toggleSwitch = document.querySelector('.theme-switch input[type=checkbox]');
-const currentTheme = localStorage.getItem('theme');
-
-if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
-
-    if (currentTheme === 'dark') {
-        toggleSwitch.checked = true;
-    }
-}
-
-const switchTheme = (e) => {
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
-    } else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'light');
-    }
-}
-
-toggleSwitch.addEventListener('change', switchTheme, false);
