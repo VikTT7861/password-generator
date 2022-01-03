81be4a8e65108d13919f05ed74c523467f6167a7
Victor Zuniga <victorandres1218@gmail.com>
2022-01-03 14:02:42 -0600

    feat(login,signup): Adding Login & Signup forms.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,6 +7,14 @@ ROUTER.get('/', (request: Request, response: Response) => {
     return response.render('index');
 });
 
+ROUTER.get('/login', (request: Request, response: Response) => {
+    return response.render('login');
+});
+
+ROUTER.get('/signup', (request: Request, response: Response) => {
+    return response.render('signup');
+});
+
 ROUTER.get('/password', (request: Request, response: Response) => {
     return response.render('password');
 });
